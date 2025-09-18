86327148ca9f520292809e5fa148e8d8ccb546cc
cdwkr <choedaewon67@gmail.com>
2025-09-18 12:50:50 +0900

    test5.c

---
+++ project_2/test5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int n;
+    printf("몇단 삼각형입니까?: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
